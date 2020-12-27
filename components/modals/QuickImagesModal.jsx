e92ce72f943a7dc442d31ee03bc0d4bc27d2c6fb
abUwUser <pedro.santos.cartaxo@gmail.com>
2020-12-26 22:10:29 -0300

    showing images now

--- components/modals/QuickImagesModal.jsx
+++ components/modals/QuickImagesModal.jsx
@@ -1,6 +1,9 @@
-import { React, getModuleByDisplayName } from "@vizality/webpack"
+import { React, getModule, getModuleByDisplayName } from "@vizality/webpack"
 import { Modal } from "@vizality/components"
 const { close: closeModal } = require('@vizality/modal')
+
+const fs = require("fs")
+const path = require("path")
 
 const FormTitle = getModuleByDisplayName('FormTitle', false)
 
@@ -13,9 +16,28 @@ module.exports = class QuickImagesModal extends React.PureComponent {
                     <Modal.CloseButton onClick={closeModal} />
                 </Modal.Header>
                 <Modal.Content>
-                    <span>yis</span>
+                    {
+                        this.openImageList()
+                    }
                 </Modal.Content>
             </Modal>
         </>
+    }
+
+    openImageList(){
+        let images = this.outputImages("D:vizality/addons/plugins/quick-images/images")
+        console.log(images)
+        return images.map((img) => {
+            return <img src={"data:image/png;base64, " + fs.readFileSync("D:vizality/addons/plugins/quick-images/images/" + img, { encoding: "base64"} )} />
+        })
+    }
+    
+    outputImages(dirname) {
+        let filenames = fs.readdirSync(dirname)
+        let images = filenames.filter(function (e) {
+            let extname = path.extname(e).toLowerCase()
+            if (extname === '.png' || extname === '.jpg') return e
+        });
+        return images
     }
 }
